3b4116de0a980ec2d4ebb6a15f46f87a879401d9
Saulo Gelinski <30400021+Sauspin@users.noreply.github.com>
2021-06-20 22:47:38 +0000

    Adição da função de juros simples.

--- investimento.cpp
+++ investimento.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 struct Tinvestimento{
   float montInvest; // capital investido
   float jurosAno;   // juros anual
-  int periodInvestAnos; período considerado em anos
-  float montSimples;
-  float montComposto;
-  float convertAnoMes;
+  int periodInvest; //período considerado em mes
+  float montSimples; // rendimento com juros simples
+  float montComposto; // rendimento com juros composto
+  float convertAnoMes; // conversão de juros Ano para mês
+  };
+
+
+  Tinvestimento saidaSimples(){
+   Tinvestimento montSimples;
+   
+  //int jurosAnoMes = Tinvestimento.jurosAno / 12;
+   float taxa = Tinvestimento.montInvest*(Tinvestimento.jurosAno/12)*Tinvestimento.periodInvest*0.01;
+
+   montSimples = Tinvestimento.montInvest + taxa; 
+
+   return montSimples;
+  }
+
   
-};
+  Tinvestimento saidaComp(){
+   Tinvestimento calcComp;
+
+
+   return calcComp;
+  }
+
+  
+  Tinvestimento convAnoMes(){
+   Tinvestimento convert;
+
+
+   return convert;
+  }
--- main.cpp
+++ main.cpp
@@ -4,25 +4,33 @@
 using namespace std;
 
 
-Tinvestimento dataIn (){
+Tinvestimento dataIn(){
  Tinvestimento obterDados;
 
  cout << "Digite o valor do Investimento: ";
  cin >> obterDados.montInvest;
- cout << "Digite a taxa de juros anual; ";
+ cout << "Digite a taxa de juros anual em %: ";
  cin >> obterDados.jurosAno;
- cout << "Digite o periodo do Investimento em anos: ";
- cin >> obterDados.periodInvestAnos;
+ cout << "Digite o periodo do Investimento em meses: ";
+ cin >> obterDados.periodInvest;
 
  return obterDados;
 }
 
+Tinvestimento dataOut(){
+ Tinvestimento plotaDados;
 
+ cout << "Montante de rendimentos Simples: " << montSimples << ;
 
-
+//return 0;
+}
 
 
 
 int main() {
+
+  dataIn();
+  saidaSimples();
+  dataOut();
   
 }
