e2da588d8e56e334672fcd02c7528aa2223b0c67
Saulo Gelinski <30400021+Sauspin@users.noreply.github.com>
2021-06-20 21:31:52 +0000

    Inserido função que obtém os dados do usuário.

---
+++ investimento.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+struct Tinvestimento{
+  float montInvest; // capital investido
+  float jurosAno;   // juros anual
+  int periodInvestAnos; período considerado em anos
+  float montSimples;
+  float montComposto;
+  float convertAnoMes;
+  
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include "investimento.cpp"
+
+using namespace std;
+
+
+Tinvestimento dataIn (){
+ Tinvestimento obterDados;
+
+ cout << "Digite o valor do Investimento: ";
+ cin >> obterDados.montInvest;
+ cout << "Digite a taxa de juros anual; ";
+ cin >> obterDados.jurosAno;
+ cout << "Digite o periodo do Investimento em anos: ";
+ cin >> obterDados.periodInvestAnos;
+
+ return obterDados;
+}
+
+
+
+
+
+
 
 int main() {
-  std::cout << "Hello World!\n";
+  
 }
